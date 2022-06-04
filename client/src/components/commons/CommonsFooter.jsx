18d71ff17f265a5c89463bd9b94e17d5df49fab5
Ethan Turner <ethanaturner13@gmail.com>
2022-06-03 18:31:31 -0700

    feat(Commons): Restructure footer for a11y (contrast, accessible link names) and responsive design

--- client/src/components/commons/CommonsFooter.jsx
+++ client/src/components/commons/CommonsFooter.jsx
@@ -1,52 +1,97 @@
 import './Commons.css';
 
 import {
-    Grid,
     Icon,
     Image
 } from 'semantic-ui-react';
 import React from 'react';
 
-const CommonsFooter = (_props) => {
+const CommonsFooter = () => {
+  const contactLinks = [
+    {
+      key: 'email',
+      href: 'mailto:info@libretexts.org',
+      text: 'Contact Us',
+      title: 'Contact Us (opens new email)',
+      icon: false,
+    },
+    {
+      key: 'facebook',
+      href: 'https://www.facebook.com/LibreTexts',
+      text: '',
+      title: 'LibreTexts on Facebook (opens in new tab)',
+      icon: true,
+      iconName: 'facebook f',
+    },
+    {
+      key: 'twitter',
+      href: 'https://twitter.com/libretexts',
+      text: 'Contact Us',
+      title: 'LibreTexts on Twitter (opens in new tab)',
+      icon: true,
+      iconName: 'twitter',
+    },
+    {
+      key: 'legal',
+      href: 'https://libretexts.org/legal/index.html',
+      text: 'Legal',
+      title: 'Legal (opens in new tab)',
+      icon: false,
+    },
+    {
+      key: 'a11y',
+      href: '/accessibility',
+      text: 'Accessibility',
+      title: 'Accessibility (opens in new tab)',
+      icon: false,
+    },
+  ];
 
-    return (
-        <Grid id='commons-footer' className='mt-4p'>
-            {(process.env.REACT_APP_ORG_ID === 'libretexts') &&
-                <Grid.Row>
-                    <Grid.Column textAlign='center'>
-                        <p>The LibreTexts libraries are supported by the United States Department of Education Open Textbook Pilot Project.</p>
-                    </Grid.Column>
-                </Grid.Row>
-            }
-            {(process.env.REACT_APP_ORG_ID === 'libretexts') &&
-                <Grid.Row>
-                    <Grid.Column textAlign='center'>
-                        <a className='commons-footer-contactlinks' href='mailto:info@libretexts.org'>Contact Us</a>
-                        <a className='commons-footer-contactlinks' href='https://www.facebook.com/LibreTexts/' target='_blank' rel='noopener noreferrer'><Icon name='facebook f' aria-label='Follow LibreTexts on Facebook' /></a>
-                        <a className='commons-footer-contactlinks' href='https://twitter.com/libretexts' target='_blank' rel='noopener noreferrer'><Icon name='twitter' aria-label='Follow LibreTexts on Twitter' /></a>
-                        <a className='commons-footer-contactlinks' href='https://libretexts.org/legal/index.html' target='_blank' rel='noopener noreferrer'>Legal</a>
-                        <a className='commons-footer-contactlinks' href='/accessibility' target='_blank' rel='noopener noreferrer'>Accessibility</a>
-                    </Grid.Column>
-                </Grid.Row>
-            }
-            {(process.env.REACT_APP_ORG_ID !== 'libretexts') &&
-                <Grid.Row>
-                    <Grid.Column>
-                        <p id='commons-poweredby-tagline' className='text-center'><em>powered by</em></p>
-                        <Image
-                            src='/transparent_logo.png'
-                            size='small'
-                            className='cursor-pointer'
-                            centered
-                            onClick={() => {
-                                window.open('https://libretexts.org', '_blank', 'noopener');
-                            }}
-                        />
-                    </Grid.Column>
-                </Grid.Row>
-            }
-        </Grid>
-    )
+  return (
+    <div
+      id="commons-footer"
+      className={process.env.REACT_APP_ORG_ID === 'libretexts' ? 'libretexts-footer' : 'libregrid-footer'}
+    >
+      {(process.env.REACT_APP_ORG_ID === 'libretexts') ? (
+        <>
+          <div className="footer-row">
+            <p>The LibreTexts libraries are supported by the United States Department of Education Open Textbook Pilot Project.</p>
+          </div>
+          <div className="footer-row footer-links">
+            {(contactLinks.map((item) => (
+              <a
+                href={item.href}
+                rel="noreferrer"
+                target="_blank"
+                title={item.title}
+                className="contact-link"
+              >
+                {item.icon ? (
+                  <Icon name={item.iconName} aria-hidden={true} />
+                ) : (
+                  <span aria-hidden={true}>{item.text}</span>
+                )}
+              </a>
+            )))}
+          </div>
+        </>
+      ) : (
+        <>
+          <p id="commons-poweredby-tagline"><em>powered by</em></p>
+          <Image
+            src="/transparent_logo.png"
+            size="small"
+            className="cursor-pointer"
+            centered
+            onClick={() => {
+              window.open('https://libretexts.org', '_blank', 'noopener');
+            }}
+            alt="LibreTexts"
+          />
+        </>
+      )} 
+    </div>
+  )
 }
 
 export default CommonsFooter;
