e41d0466d301ef5cc914f47dd464df45c34520a3
Ethan Turner <ethanaturner13@gmail.com>
2022-09-15 21:48:29 -0700

    refactor(client): Cleanup and standardize PrivateRoute definition

--- client/src/components/util/PrivateRoute.jsx
+++ client/src/components/util/PrivateRoute.jsx
@@ -1,26 +1,32 @@
-//
-// PrivateRoute
-//  Specifies a route in which a user SHOULD be authenticated
-//  If the user is authenticated, they should be redirected to login
-//
-
-import { Route } from 'react-router-dom';
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router-dom';
 import AuthHelper from './AuthHelper.js';
 
+/**
+ * A route in which the user SHOULD be authenticated. If the user is not
+ * authenticated, they are redirected to login.
+ */
 const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={(props) => {
-        if (AuthHelper.isAuthenticated() === true) {
-            return (<Component {...props} />)
-        } else {
-            let redirectURI = encodeURIComponent(props.location.pathname);
-            if (props.location.search !== '') {
-                redirectURI += encodeURIComponent(props.location.search);
-            }
-            window.location.assign(`/login?redirect_uri=${redirectURI}`);
-        }
-    }} />
+  <Route {...rest} render={(props) => {
+    if (AuthHelper.isAuthenticated()) {
+      return (<Component {...props} />)
+    }
+
+    let redirectURI = props.location.pathname;
+    if (props.location.search) {
+      redirectURI = `${redirectURI}${props.location.search}`;
+    }
+    redirectURI = encodeURIComponent(redirectURI);
+    return <Redirect to={{ pathname: '/login', search: `?redirect_uri=${redirectURI}` }} />;
+  }} />
 );
+
+PrivateRoute.propTypes = {
+  /**
+   * The component to render if the user is authenticated.
+   */
+  component: PropTypes.elementType.isRequired,
+};
 
 export default PrivateRoute;
