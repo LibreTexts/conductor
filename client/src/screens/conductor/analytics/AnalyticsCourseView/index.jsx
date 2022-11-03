aa7aac9ba24ab87417d06887a1229eaedb738af8
Ethan Turner <ethanaturner13@gmail.com>
2022-11-03 16:54:02 -0700

    feat(Analytics): Update Course View to prevent overflow and double scrollbars on Dashboard view

--- client/src/screens/conductor/analytics/AnalyticsCourseView/index.jsx
+++ client/src/screens/conductor/analytics/AnalyticsCourseView/index.jsx
@@ -7,6 +7,7 @@ import AnalyticsCourseSettings from 'components/analytics/AnalyticsCourseSetting
 import LearningAnalyticsDashboard from 'components/analytics/LearningAnalyticsDashboard';
 import useQueryParam from 'utils/useQueryParam';
 import useGlobalError from 'components/error/ErrorHooks';
+import styles from './AnalyticsCourseView.module.css';
 
 /**
  * The main entry point to an Analytics Course, presenting the user with the Analytics Dashboard.
@@ -125,7 +126,54 @@ const AnalyticsCourseView = () => {
   }
 
   return (
-    <Grid className="controlpanel-container" divided="vertically">
+    <div id={styles.wrapper}>
+      <div id={styles.header_row}>
+        <Header as="h2" className="component-header">{course.title}</Header>
+      </div>
+      <div id={styles.content_row}>
+        <Segment.Group id={styles.segments_wrapper} className={activePane === 'dashboard' ? styles.dashboard_active : ''}>
+          <Segment className="flex-row-div">
+            <div className="left-flex">
+              <Breadcrumb>
+                <Breadcrumb.Section as={Link} to="/analytics">Analytics</Breadcrumb.Section>
+                <Breadcrumb.Divider icon="right chevron" />
+                <Breadcrumb.Section active>{course.title}</Breadcrumb.Section>
+              </Breadcrumb>
+            </div>
+            <div className="right-flex">
+              <Loader active={loading} inline className="mr-2e" />
+              <Button
+                onClick={handleSettingsClick}
+                color={activePane === 'settings' ? undefined : 'blue'}
+              >
+                <Icon name={activePane === 'settings' ? 'close' : 'settings'} />
+                {activePane === 'settings' && 'Close'} Course Settings
+              </Button>
+            </div>
+          </Segment>
+          <Segment id={styles.active_pane_segment}>
+            {showCourseCreated && (
+              <Message icon success className="mb-1e">
+                <Icon name="check circle" />
+                <Message.Content>
+                  <Message.Header>Course Created</Message.Header>
+                  <p>Welcome to your new Analytics Course, <em>{course.title}</em>!</p>
+                </Message.Content>
+              </Message>
+            )}
+            <ActivePane />
+          </Segment>
+        </Segment.Group>
+      </div>
+    </div>
+  );
+};
+
+export default AnalyticsCourseView;
+
+/*
+
+    <Grid id={styles.wrapper} divided="vertically">
       <Grid.Row columns={1}>
         <Grid.Column>
           <Header as="h2" className="component-header">{course.title}</Header>
@@ -133,45 +181,9 @@ const AnalyticsCourseView = () => {
       </Grid.Row>
       <Grid.Row columns={1}>
         <Grid.Column>
-          <Segment.Group>
-            <Segment className="flex-row-div">
-              <div className="left-flex">
-                <Breadcrumb>
-                  <Breadcrumb.Section as={Link} to="/analytics">Analytics</Breadcrumb.Section>
-                  <Breadcrumb.Divider icon="right chevron" />
-                  <Breadcrumb.Section active>{course.title}</Breadcrumb.Section>
-                </Breadcrumb>
-              </div>
-              <div className="center-flex">
-                <Loader active={loading} inline="centered" />
-              </div>
-              <div className="right-flex">
-                <Button
-                  onClick={handleSettingsClick}
-                  color={activePane === 'settings' ? undefined : 'blue'}
-                >
-                  <Icon name={activePane === 'settings' ? 'close' : 'settings'} />
-                  {activePane === 'settings' && 'Close'} Course Settings
-                </Button>
-              </div>
-            </Segment>
-            <Segment>
-              {showCourseCreated && (
-                <Message icon success className="mb-1e">
-                  <Icon name="check circle" />
-                  <Message.Content>
-                    <Message.Header>Course Created</Message.Header>
-                    <p>Welcome to your new Analytics Course, <em>{course.title}</em>!</p>
-                  </Message.Content>
-                </Message>
-              )}
-              <ActivePane />
-            </Segment>
-          </Segment.Group>
+
         </Grid.Column>
       </Grid.Row>
     </Grid>
-  );
-};
 
-export default AnalyticsCourseView;
+*/
