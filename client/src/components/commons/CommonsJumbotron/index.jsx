2eb0ef5f08a14391640c395e550ec5af3ebf4a69
Ethan Turner <ethanaturner13@gmail.com>
2023-02-15 21:46:14 -0800

    fix(Commons): Remove text from jumbotron

--- client/src/components/commons/CommonsJumbotron/index.jsx
+++ client/src/components/commons/CommonsJumbotron/index.jsx
@@ -3,36 +3,21 @@ import PropTypes from 'prop-types';
 import './CommonsJumbotron.css';
 
 /**
- * The Jumbotron is displayed at the top of the Commons interfaces, display information about
- * the instance Organization over their preferred background image.
+ * The Jumbotron is displayed at the top of the Commons interfaces, featuring an Organization's
+ * preferred background image.
  */
-const CommonsJumbotron = ({ mainHeader, subHeader, backgroundURL }) => {
+const CommonsJumbotron = ({ backgroundURL }) => {
 
   const style = {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.45), rgba(0, 0, 0, 0.45)), url(${backgroundURL})`,
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.15), rgba(0, 0, 0, 0.15)), url(${backgroundURL})`,
   };
 
   return (
-    <div id="commons-jumbotron" style={style}>
-      <div>
-        <h1 id="commons-libreheader">
-          <span id="commons-libreheader-sub">{subHeader}</span>
-          <span id="commons-libreheader-main">{mainHeader}</span>
-        </h1>
-      </div>
-    </div>
+    <div id="commons-jumbotron" style={style}></div>
   )
 };
 
 CommonsJumbotron.propTypes = {
-  /**
-   * The display title of the Organization's Commons.
-   */
-  mainHeader: PropTypes.string.isRequired,
-  /**
-   * Short text to display above the main header.
-   */
-  subHeader: PropTypes.string.isRequired,
   /**
    * A URL pointing to an image to use as the Jumbotron background.
    */
