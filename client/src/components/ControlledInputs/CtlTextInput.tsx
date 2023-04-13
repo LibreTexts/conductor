14a99e0b6275c46d8ec0d218d5aa7a080714415b
Jake Turner <52841588+jakeaturner@users.noreply.github.com>
2023-04-12 21:42:11 -0700

    fix(ControlledInputs): rework text input for improved ref handling (#49)

---
+++ client/src/components/ControlledInputs/CtlTextInput.tsx
@@ -0,0 +1,38 @@
+import { FieldValues, FieldPath, Controller } from "react-hook-form";
+import { Form, FormInputProps } from "semantic-ui-react";
+import { ControlledInputProps } from "../../types";
+
+/**
+ * Semantic UI Form.Input component wrapped in react-hook-form controller
+ * Fall-through props allow for finer-grained control and styling on a case-by-case basis
+ */
+export default function CtlTextInput<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({
+  name,
+  control,
+  rules,
+  ...rest
+}: ControlledInputProps<TFieldValues, TName> & FormInputProps) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({
+        field: { value, onChange, onBlur },
+        fieldState: { error },
+      }) => (
+        <Form.Input
+          value={value}
+          onChange={onChange}
+          onBlur={onBlur}
+          error={error?.message}
+          
+          {...rest}
+        />
+      )}
+    />
+  );
+}
