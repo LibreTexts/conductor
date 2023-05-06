f15f09b5fca4b6e70df012b6e275109b2d120213
jakeaturner <turnerj804@gmail.com>
2023-05-05 21:39:18 -0700

    fix: server model typings

--- server/models/peerreviewrubric.ts
+++ server/models/peerreviewrubric.ts
@@ -5,39 +5,31 @@ export interface PeerReviewRubricInterface extends Document {
   rubricID: string;
   isOrgDefault: boolean;
   rubricTitle: string;
-  headings: [
-    {
-      text: string;
-      order: number;
-    }
-  ];
-  textBlocks: [
-    {
-      text: string;
-      order: number;
-    }
-  ];
-  prompts: [
-    {
-      promptType:
-        | "3-likert"
-        | "5-likert"
-        | "7-likert"
-        | "text"
-        | "dropdown"
-        | "checkbox";
-      promptText: string;
-      promptRequired: boolean;
-      promptOptions?: [
-        {
-          key?: string;
-          value?: string;
-          text?: string;
-        }
-      ];
-      order: number;
-    }
-  ];
+  headings: {
+    text: string;
+    order: number;
+  }[];
+  textBlocks: {
+    text: string;
+    order: number;
+  }[];
+  prompts: {
+    promptType:
+      | "3-likert"
+      | "5-likert"
+      | "7-likert"
+      | "text"
+      | "dropdown"
+      | "checkbox";
+    promptText: string;
+    promptRequired: boolean;
+    promptOptions?: {
+      key?: string;
+      value?: string;
+      text?: string;
+    }[];
+    order: number;
+  }[];
 }
 
 const PeerReviewRubricSchema = new Schema<PeerReviewRubricInterface>(
