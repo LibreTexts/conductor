5e9b9ac61f0278d92aca73218d768e1d6e4da545
Ethan Turner <ethanaturner13@gmail.com>
2023-06-27 21:15:37 -0700

    fix(API): remove deprecated Events Manager endpoint

--- server/api.js
+++ server/api.js
@@ -1314,15 +1314,6 @@ router.route('/orgevents/:eventID/participants/download')
   orgEventsAPI.downloadParticipantData
 )
 
-router.route("/orgevents/:eventID/participants/:participantID")
-.delete(
-  authAPI.verifyRequest,
-  authAPI.getUserAttributes,
-  orgEventsAPI.validate("unregisterParticipant"),
-  middleware.checkValidationErrors,
-  orgEventsAPI.unregisterParticipant
-);
-
 router.route('/orgevents/:eventID/register')
 .post(
   authAPI.verifyRequest,
