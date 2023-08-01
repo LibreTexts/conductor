5ee444a1f4b667aa54cc1089a9700737811ce3bf
Jake Turner <52841588+jakeaturner@users.noreply.github.com>
2023-07-31 21:47:20 -0700

    refactor(Navbar): refactor libraries dropdown (#108)

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,76 @@
+export const LIBRARIES: {
+  href: string;
+  shortName: string;
+  longName: string;
+}[] = [
+  {
+    href: "https://bio.libretexts.org/",
+    shortName: "bio",
+    longName: "Biology",
+  },
+  {
+    href: "https://biz.libretexts.org/",
+    shortName: "biz",
+    longName: "Business",
+  },
+  {
+    href: "https://chem.libretexts.org/",
+    shortName: "chem",
+    longName: "Chemistry",
+  },
+  {
+    href: "https://eng.libretexts.org/",
+    shortName: "eng",
+    longName: "Engineering",
+  },
+  {
+    href: "https://espanol.libretexts.org/",
+    shortName: "espanol",
+    longName: "Español",
+  },
+  {
+    href: "https://geo.libretexts.org/",
+    shortName: "geo",
+    longName: "Geosciences",
+  },
+  {
+    href: "https://human.libretexts.org/",
+    shortName: "human",
+    longName: "Humanities",
+  },
+  {
+    href: "https://k12.libretexts.org/",
+    shortName: "k12",
+    longName: "K12 Education",
+  },
+  {
+    href: "https://math.libretexts.org/",
+    shortName: "math",
+    longName: "Mathematics",
+  },
+  {
+    href: "https://med.libretexts.org/",
+    shortName: "med",
+    longName: "Medicine",
+  },
+  {
+    href: "https://phys.libretexts.org/",
+    shortName: "phys",
+    longName: "Physics",
+  },
+  {
+    href: "https://socialsci.libretexts.org/",
+    shortName: "socialsci",
+    longName: "Social Science",
+  },
+  {
+    href: "https://stats.libretexts.org/",
+    shortName: "stats",
+    longName: "Statistics",
+  },
+  {
+    href: "https://workforce.libretexts.org/",
+    shortName: "workforce",
+    longName: "Workforce",
+  },
+];
