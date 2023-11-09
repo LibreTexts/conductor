00f3041dce4747398fdde07579bfd24752230a84
Jake Turner <turnerj804@gmail.com>
2023-11-09 10:23:20 -0800

    feat(LLAD): [wip] use new search API

--- client/src/components/commons/CommonsCatalog/CatalogCard/index.tsx
+++ client/src/components/commons/CommonsCatalog/CatalogCard/index.tsx
@@ -1,29 +1,38 @@
 import { Link } from "react-router-dom";
 import { Card, CardProps } from "semantic-ui-react";
-import { Book, ProjectFile } from "../../../../types";
-import { isBook, isProjectFile } from "../../../../utils/typeHelpers";
+import { Book, ProjectFileWProjectID } from "../../../../types";
+import { isBook } from "../../../../utils/typeHelpers";
 import BookCardContent from "./BookCardContent";
 import FileCardContent from "./FileCardContent";
+import { downloadFile } from "../../../../utils/assetHelpers";
+import useGlobalError from "../../../error/ErrorHooks";
 
 interface CatalogCardProps extends CardProps {
-  item: Book | ProjectFile;
+  item: Book | ProjectFileWProjectID;
 }
 
 const CatalogCard: React.FC<CatalogCardProps> = ({ item, ...props }) => {
-  const getItemURL = (item: Book | ProjectFile) => {
-    if (isBook(item)) {
-      return `/book/${item.bookID}`;
-    }
-    if (isProjectFile(item)) {
-      return `/file/${item.fileID}`;
-    }
-    return "";
+  const { handleGlobalError } = useGlobalError();
+  const getBookURL = (item: Book) => {
+    return `/book/${item.bookID}`;
   };
+
+  async function handleFileDownload(file: ProjectFileWProjectID) {
+    let success = false;
+    try {
+      success = await downloadFile(file.projectID, file.fileID);
+    } catch (err) {
+      if (!success) {
+        handleGlobalError("Unable to download file. Please try again later.");
+      }
+    }
+  }
 
   return (
     <Card
-      as={Link}
-      to={getItemURL(item)}
+      as={isBook(item) ? Link : Card}
+      to={isBook(item) ? getBookURL(item) : ""}
+      onClick={isBook(item) ? undefined : () => handleFileDownload(item)}
       className="commons-content-card"
       {...props}
     >
--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -19,6 +19,7 @@ import { a11ySectionReviewSchema } from "./a11y";
 import {
   Project,
   ProjectFile,
+  ProjectFileWProjectID,
   ProjectTag,
   ProjectClassification,
   ProjectStatus,
@@ -127,6 +128,7 @@ export type {
   a11ySectionReviewSchema,
   Project,
   ProjectFile,
+  ProjectFileWProjectID,
   ProjectTag,
   CIDDescriptor,
   User,
