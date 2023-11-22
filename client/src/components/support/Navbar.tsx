0944c8649ac422bef43f91268448ca1a149c0c1a
Jake Turner <turnerj804@gmail.com>
2023-11-21 17:34:39 -0800

    feat(Insight): add insight search to support center navbar

--- client/src/components/support/Navbar.tsx
+++ client/src/components/support/Navbar.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import withUserStateDependency from "../../enhancers/withUserStateDependency.jsx";
-import { Button, Icon, Image } from "semantic-ui-react";
+import { Button, Form, Icon, Image } from "semantic-ui-react";
 import Launchpad from "../navigation/Launchpad.js";
 import { useTypedSelector } from "../../state/hooks.js";
+import { useMediaQuery } from "react-responsive";
 
 const SupportCenterNavbar: React.FC<{}> = () => {
   const user = useTypedSelector((state) => state.user);
   const [isStaff, setIsStaff] = useState(false);
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
+
+  const isTailwindXl = useMediaQuery(
+    { minWidth: 1280 }, // Tailwind XL breakpoint
+    undefined
+  );
 
   useEffect(() => {
     if (user && user.isSuperAdmin) {
@@ -15,24 +23,60 @@ const SupportCenterNavbar: React.FC<{}> = () => {
     }
   }, [user]);
 
+  useEffect(() => {
+    // dont show search bar on /insight
+    const split = window.location.href.split("/");
+    const last = split[split.length - 1];
+    if (last === "insight") {
+      setShowSearch(false);
+    } else {
+      setShowSearch(true);
+    }
+  }, [window.location.href]);
+
+  const handleSearch = () => {
+    if (!search) return;
+    window.location.href = `/insight/search?query=${encodeURIComponent(
+      search
+    )}`;
+  };
+
   return (
     <div className="flex flex-row bg-white h-fit py-2 px-4 shadow-md border-b items-center justify-between">
-      <div className="flex flex-row">
+      <div className="flex flex-row w-4/5">
         <div className="flex ml-2 mt-0.5">
           <Launchpad />
         </div>
-        <div
-          className="flex flex-row items-center cursor-pointer"
-          onClick={() => window.location.assign("/support")}
+        {isTailwindXl && (
+          <div
+            className="flex flex-row items-center cursor-pointer"
+            onClick={() => window.location.assign("/support")}
+          >
+            <Image
+              src="https://cdn.libretexts.net/Logos/libretexts_full.png"
+              className="h-12 ml-6"
+            />
+            <span className="hidden lg:flex ml-2 text-2xl font-semibold">
+              | Support Center
+            </span>
+          </div>
+        )}
+        <Form
+          className="ml-8 w-3/5 xl:1/3 mt-1"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
         >
-          <Image
-            src="https://cdn.libretexts.net/Logos/libretexts_full.png"
-            className="h-12 ml-6"
-          />
-          <span className="hidden lg:flex ml-2 text-2xl font-semibold">
-            | Support Center
-          </span>
-        </div>
+          {showSearch && (
+            <Form.Input
+              placeholder="Search LibreTexts Insight..."
+              icon="search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          )}
+        </Form>
       </div>
       <div className="flex">
         {/* {user && (
