3e7c1ffbc119ade199947070650a46984a8ce156
jakeaturner <jturner@cosmistack.com>
2023-12-20 13:21:09 -0800

    fix(Assets): debug bulk download

--- server/api/projects.js
+++ server/api/projects.js
@@ -534,6 +534,7 @@ async function uploadProjectThumbnail(req, res) {
     }
 
     const url = assembleUrl([
+      'https://',
       process.env.AWS_PROJECT_THUMBNAILS_DOMAIN,
       thumbnailKey,
     ]);
@@ -2990,8 +2991,9 @@ async function bulkDownloadProjectFiles(req, res) {
         fileKeys.push(fileKey);
       });
 
+      console.log('[SYSTEM] Creating ZIP and notifying user...')
       createZIPAndNotify(fileKeys, foundFiles, emailToNotify); // Don't await, just run in background and return success
-
+      
       res.setHeader('content-type', 'application/json');
       return res.send({
         err: false,
