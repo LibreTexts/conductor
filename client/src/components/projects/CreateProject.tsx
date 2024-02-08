587ca2647529a8293eaad6fc30aac0e735ae522e
jakeaturner <jturner@cosmistack.com>
2024-02-08 14:13:55 -0800

    feat(Conductor): add create project opt to Home

--- client/src/components/projects/CreateProject.tsx
+++ client/src/components/projects/CreateProject.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import axios from 'axios';
-import { Button, Form, Icon, Modal } from 'semantic-ui-react';
-import { visibilityOptions } from '../util/ProjectHelpers';
-import useGlobalError from '../error/ErrorHooks';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { Button, Form, Icon, Modal } from "semantic-ui-react";
+import { visibilityOptions } from "../util/ProjectHelpers";
+import useGlobalError from "../error/ErrorHooks";
+import { useTypedSelector } from "../../state/hooks";
+
+interface CreateProjectProps {
+  show: boolean;
+  onClose: () => void;
+}
 
 /**
  * Modal tool to create a new Project.
  */
-const CreateProject = ({ show, onClose }) => {
-
+const CreateProject: React.FC<CreateProjectProps> = ({ show, onClose }) => {
   // Global state and error handling
   const { handleGlobalError } = useGlobalError();
   const history = useHistory();
-  const org = useSelector((state) => state.org);
+  const org = useTypedSelector((state) => state.org);
 
   // Form Data
-  const [projTitle, setProjTitle] = useState('');
-  const [projVis, setProjVis] = useState('private');
+  const [projTitle, setProjTitle] = useState("");
+  const [projVis, setProjVis] = useState("private");
 
   // Form State
   const [loading, setLoading] = useState(false);
@@ -55,15 +58,17 @@ const CreateProject = ({ show, onClose }) => {
     if (validateForm()) {
       try {
         setLoading(true);
-        const createRes = await axios.post('/project', {
+        const createRes = await axios.post("/project", {
           title: projTitle,
           visibility: projVis,
         });
         if (createRes.data.err) {
-          throw (new Error(createRes.data.errMsg));
+          throw new Error(createRes.data.errMsg);
         }
         if (createRes.data.projectID) {
-          history.push(`/projects/${createRes.data.projectID}?projectCreated=true`);
+          history.push(
+            `/projects/${createRes.data.projectID}?projectCreated=true`
+          );
         } else {
           history.push(`/projects?projectCreated=true`);
         }
@@ -74,31 +79,14 @@ const CreateProject = ({ show, onClose }) => {
     }
   }
 
-  /**
-   * Updates the new Title in state.
-   *
-   * @param {React.ChangeEvent<HTMLInputElement>} e - Event that activated the handler.
-   */
-  function handleProjectTitleChange(e) {
-    setProjTitle(e.target.value);
-  }
-
-  /**
-   * Updates the new Visibility setting in state.
-   *
-   * @param {React.ChangeEvent} _e - Event that activated the handler. 
-   * @param {object} data - Date passed from the UI element.
-   * @param {string} data.value - The new visibility setting selection. 
-   */
-  function handleProjectVisibilityChange(_e, { value }) {
-    setProjVis(value);
-  }
-
   return (
     <Modal size="large" open={show} onClose={onClose}>
       <Modal.Header>Create Project</Modal.Header>
       <Modal.Content>
-        <p>This project will be created within <strong>{org.name}</strong>. You can add tags, collaborators, and more after creation.</p>
+        <p>
+          This project will be created within <strong>{org.name}</strong>. You
+          can add tags, collaborators, and more after creation.
+        </p>
         <Form noValidate onSubmit={createProject}>
           <Form.Input
             fluid
@@ -107,7 +95,7 @@ const CreateProject = ({ show, onClose }) => {
             required
             type="text"
             value={projTitle}
-            onChange={handleProjectTitleChange}
+            onChange={(e) => setProjTitle(e.target.value)}
             error={titleErr}
           />
           <Form.Select
@@ -115,7 +103,7 @@ const CreateProject = ({ show, onClose }) => {
             label="Project Visibility"
             placeholder="Visibility..."
             options={visibilityOptions}
-            onChange={handleProjectVisibilityChange}
+            onChange={(e, { value }) => setProjVis(value as string)}
             value={projVis}
           />
         </Form>
@@ -129,21 +117,6 @@ const CreateProject = ({ show, onClose }) => {
       </Modal.Actions>
     </Modal>
   );
-};
-
-CreateProject.propTypes = {
-  /**
-   * Opens or closes the modal.
-   */
-  show: PropTypes.bool.isRequired,
-  /**
-   * Handler to activate when the modal is closed.
-   */
-  onClose: PropTypes.func,
-};
-
-CreateProject.defaultProps = {
-  onClose: () => { },
 };
 
 export default CreateProject;
