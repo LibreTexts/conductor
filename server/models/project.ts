3f81b47db9447b919683f510492bb8d547e80e9e
jakeaturner <jturner@cosmistack.com>
2024-05-01 16:18:51 -0700

    fix(Project): add MongoDB recommended indices

--- server/models/project.ts
+++ server/models/project.ts
@@ -391,6 +391,10 @@ const ProjectSchema = new Schema<ProjectInterface>(
 );
 
 ProjectSchema.index({ title: "text" });
+ProjectSchema.index({members: 1, updatedAt: -1, title: -1, status: 1, projectID: 1})
+ProjectSchema.index({leads: 1, updatedAt: -1, title: -1, status: 1, projectID: 1})
+ProjectSchema.index({liaisons: 1, updatedAt: -1, title: -1, status: 1, projectID: 1})
+ProjectSchema.index({auditors: 1, updatedAt: -1, title: -1, status: 1, projectID: 1})
 
 ProjectSchema.virtual("defaultPrimaryAuthor", {
   ref: "Author",
