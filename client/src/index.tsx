cd9265155bbae5db5d8f4cdc2ad16e7ccc675155
Jake Turner <jturner@cosmistack.com>
2024-09-30 13:07:00 -0700

    feat!: upgrade to react 18

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import Platform from "./Platform";
+import "semantic-ui-css/semantic.min.css";
+import "./styles/index.css";
+
+/**
+ * Main React entrypoint
+ */
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<Platform />);
