7898dcc10ecca2bbd7d26b22e33d0d02c06c1e14
Jake Turner <jturner@cosmistack.com>
2024-10-03 17:18:07 -0700

    fix(LibreOne): improve verification request workflow

--- client/src/components/controlpanel/CentralIdentity/ManageVerificationRequestModal.tsx
+++ client/src/components/controlpanel/CentralIdentity/ManageVerificationRequestModal.tsx
@@ -1,15 +1,27 @@
 import "../../../styles/global.css";
 import "./ManageUserModal.css";
-import { Modal, Button, Icon, ModalProps } from "semantic-ui-react";
+import {
+  Modal,
+  Button,
+  Icon,
+  ModalProps,
+  Form,
+  Message,
+  Checkbox,
+  Divider,
+} from "semantic-ui-react";
 import { useState, useEffect } from "react";
-import { CentralIdentityUser } from "../../../types";
 import axios from "axios";
 import useGlobalError from "../../error/ErrorHooks";
 import LoadingSpinner from "../../LoadingSpinner";
-import { CentralIdentityVerificationRequest } from "../../../types/CentralIdentity";
+import {
+  CentralIdentityApp,
+  CentralIdentityVerificationRequest,
+} from "../../../types/CentralIdentity";
 import DenyVerificationRequestModal from "./DenyVerificationRequestModal";
 import DOMPurify from "dompurify";
-import ApproveVerificationRequestModal from "./ApproveVerificationRequestModal";
+import { useQuery } from "@tanstack/react-query";
+import api from "../../../api";
 
 interface ManageVerificationRequestModalProps extends ModalProps {
   show: boolean;
@@ -27,11 +39,27 @@ const ManageVerificationRequestModal: React.FC<
 
   // Data & UI
   const [loading, setLoading] = useState<boolean>(false);
-  const [user, setUser] = useState<CentralIdentityUser>();
   const [request, setRequest] = useState<CentralIdentityVerificationRequest>();
-  const [showApproveModal, setShowApproveModal] = useState<boolean>(false);
   const [showDenyModal, setShowDenyModal] = useState<boolean>(false);
   const [denyType, setDenyType] = useState<"deny" | "request_change">("deny");
+  const [approvedApps, setApprovedApps] = useState<CentralIdentityApp[]>([]);
+
+  const { data: allApps, isFetching: allAppsLoading } = useQuery<
+    CentralIdentityApp[]
+  >({
+    queryKey: ["central-identity-apps"],
+    queryFn: async () => {
+      const res = await api.getCentralIdentityApps();
+      if (res.data.err) {
+        handleGlobalError(res.data.errMsg);
+        return [];
+      }
+      return res.data.applications || [];
+    },
+    staleTime: Infinity,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
 
   // Effects
   useEffect(() => {
@@ -41,10 +69,13 @@ const ManageVerificationRequestModal: React.FC<
 
     // Reset editing states when modal is closed
     if (!show) {
-      setUser(undefined);
       setRequest(undefined);
     }
   }, [show, userId, requestId]);
+
+  useEffect(() => {
+    initApprovedApps();
+  }, [request, allApps]);
 
   // Handlers & Methods
   async function loadVerificationRequest() {
@@ -78,6 +109,87 @@ const ManageVerificationRequestModal: React.FC<
     setShowDenyModal(true);
   }
 
+  function initApprovedApps() {
+    setLoading(true);
+    if (!request) {
+      setLoading(false);
+      return;
+    }
+
+    const requestApps = request?.access_request?.applications || [];
+
+    setApprovedApps([
+      ...requestApps,
+      ...(allApps
+        ? allApps.filter((app) => {
+            return app.is_default_library === true;
+          })
+        : []),
+    ]);
+
+    setLoading(false);
+  }
+
+  async function handleApprove() {
+    try {
+      if (!requestId) return;
+
+      setLoading(true);
+
+      const res = await axios.patch(
+        `/central-identity/verification-requests/${requestId}`,
+        {
+          request: {
+            effect: "approve",
+            approved_applications: approvedApps.map((app) => app.id),
+          },
+        }
+      );
+      if (res.data.err) {
+        handleGlobalError(res.data.err);
+        return;
+      }
+
+      onSave();
+    } catch (err) {
+      handleGlobalError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  const isChecked = (appId: number) => {
+    if (!approvedApps || approvedApps.length === 0) return false;
+    return approvedApps.map((app) => app.id).includes(appId);
+  };
+
+  function handleCheckApp(appId: number) {
+    const foundApp = allApps?.find((app) => app.id === appId);
+    if (!foundApp) return;
+
+    if (isChecked(appId)) {
+      setApprovedApps([...approvedApps.filter((app) => app.id !== appId)]);
+    } else {
+      setApprovedApps([...approvedApps, foundApp]);
+    }
+  }
+
+  function handleToggleAllLibs() {
+    const defaultLibs = allApps?.filter((app) => app.is_default_library) || [];
+    const selectedNotDefaultLibs = approvedApps.filter(
+      (app) => !app.is_default_library
+    ); // Don't override selected libs that aren't default
+
+    if (defaultLibs.length === 0) return; // No default libraries to select
+
+    // if any default libs have already been selected, remove them. Otherwise, add all default libs
+    if (defaultLibs.some((app) => isChecked(app.id))) {
+      setApprovedApps([...selectedNotDefaultLibs]);
+    } else {
+      setApprovedApps([...selectedNotDefaultLibs, ...defaultLibs]);
+    }
+  }
+
   return (
     <Modal open={show} onClose={onClose} {...rest} size="large">
       <Modal.Header>Manage Verification Request</Modal.Header>
@@ -88,58 +200,121 @@ const ManageVerificationRequestModal: React.FC<
           </div>
         )}
         {!loading && (
-          <div className="flex-row-div mx-05r">
-            <div className="flex-col-div" style={{ flexBasis: "50%" }}>
-              <div className="flex-row-div flex-row-verticalcenter mb-2r">
-                <p>
-                  <strong>Name: </strong>
-                  <a
-                    href={`/controlpanel/libreone/users?user_id=${request?.user.uuid}`}
-                    target="_blank"
-                  >
-                    {request?.user.first_name} {request?.user.last_name}{" "}
-                    <Icon name="external" />
-                  </a>
-                </p>
+          <>
+            <div className="flex flex-col">
+              <div className="flex flex-row">
+                <div className="flex basis-1/2">
+                  <p>
+                    <strong>Name: </strong>
+                    <a
+                      href={`/controlpanel/libreone/users?user_id=${request?.user.uuid}`}
+                      target="_blank"
+                    >
+                      {request?.user.first_name} {request?.user.last_name}{" "}
+                      <Icon name="external" size="small" />
+                    </a>
+                  </p>
+                </div>
+                <div className="flex basis-1/2">
+                  <p className="mb-2r">
+                    <strong>Email: </strong>
+                    {request?.user.email}
+                  </p>
+                </div>
               </div>
-              <p className="mb-2r">
-                <strong>Email: </strong>
-                {request?.user.email}
-              </p>
-              <div className="flex-col-div mb-1r">
-                <p>
-                  <strong>Bio URL: </strong>
-                  <a
-                    href={DOMPurify.sanitize(request?.bio_url || "")}
-                    target="_blank"
-                  >
-                    {DOMPurify.sanitize(request?.bio_url || "")}{" "}
-                    <Icon name="external" />
-                  </a>
-                </p>
-                <p className="muted-text" style={{ fontSize: "0.9rem" }}>
-                  <em>
-                    Use caution when opening links. Do not login to any accounts
-                    or download any files from untrusted sources.
-                  </em>
-                </p>
+              <div className="flex-col-div">
+                <div className="flex-col-div mb-1r">
+                  <p>
+                    <strong>Bio URL: </strong>
+                    <a
+                      href={DOMPurify.sanitize(request?.bio_url || "")}
+                      target="_blank"
+                    >
+                      {DOMPurify.sanitize(request?.bio_url || "")}{" "}
+                      <Icon name="external" size="small" />
+                    </a>
+                  </p>
+                  <p className="muted-text" style={{ fontSize: "0.9rem" }}>
+                    <em>
+                      Use caution when opening links. Do not login to any
+                      accounts or download any files from untrusted sources.
+                    </em>
+                  </p>
+                </div>
               </div>
             </div>
-            <div
-              className="flex-col-div mb-1r ml-1r"
-              style={{ flexBasis: "50%" }}
-            >
+            <Divider />
+            <div className="">
+              <Message info icon size="mini">
+                <Icon name="info circle" />
+                <Message.Content>
+                  Note: Accounts for applications not yet integrated with
+                  LibreOne will not be automatically appropriated.
+                </Message.Content>
+              </Message>
               <p>
-                <strong>Requested Apps/Libraries:</strong>
+                The applications {request?.user.first_name}{" "}
+                {request?.user.last_name} requested have been pre-selected.
+                Select/unselect applications as necessary if you want to provide
+                access differenty than the user requested. The user will be
+                notified by email of what applications they were approved for.
               </p>
-              <ul>
-                {request?.access_request?.applications?.map((app) => (
-                  <li key={app.id}>{app.name}</li>
-                ))}
-                <li key='default'><em>Default Libraries</em></li>
-              </ul>
+              {allAppsLoading && <LoadingSpinner />}
+              {!allAppsLoading && (
+                <Form className="flex-col-div mt-4" noValidate>
+                  {allApps &&
+                    allApps
+                      .filter(
+                        (app) =>
+                          app.is_default_library === false &&
+                          app.default_access !== "all"
+                      )
+                      .map((app) => (
+                        <Checkbox
+                          key={crypto.randomUUID()}
+                          label={app.name}
+                          checked={isChecked(app.id)}
+                          onChange={() => handleCheckApp(app.id)}
+                          className="mb-1r"
+                        />
+                      ))}
+                  <div className="mt-2">
+                    <div className="flex flex-row justify-between items-center">
+                      <p>
+                        <strong>Change Default Libraries</strong>
+                      </p>
+                      <p
+                        className="underline cursor-pointer"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleToggleAllLibs();
+                        }}
+                      >
+                        {approvedApps.filter((app) => app.is_default_library)
+                          .length > 0
+                          ? "Unselect All"
+                          : "Select All"}
+                      </p>
+                    </div>
+                    <div className="flex-col-div mt-4">
+                      {allApps &&
+                        allApps
+                          .filter((app) => app.is_default_library)
+                          .map((app) => (
+                            <Checkbox
+                              key={crypto.randomUUID()}
+                              label={app.name}
+                              checked={isChecked(app.id)}
+                              onChange={() => handleCheckApp(app.id)}
+                              className="mb-1r"
+                            />
+                          ))}
+                    </div>
+                  </div>
+                </Form>
+              )}
             </div>
-          </div>
+          </>
         )}
       </Modal.Content>
       <Modal.Actions className="flex-row-div justify-between">
@@ -165,22 +340,18 @@ const ManageVerificationRequestModal: React.FC<
           <Button
             color="green"
             loading={loading}
-            onClick={() => setShowApproveModal(true)}
+            onClick={() => handleApprove()}
           >
             <Icon name="save" />
-            Approve & Select Apps
+            Approve
           </Button>
         </div>
         <div>
-          <Button onClick={handleCancel}>Cancel</Button>
+          <Button onClick={handleCancel} loading={loading}>
+            Cancel
+          </Button>
         </div>
       </Modal.Actions>
-      <ApproveVerificationRequestModal
-        show={showApproveModal}
-        requestId={requestId}
-        onSave={() => onSave()}
-        onCancel={() => setShowApproveModal(false)}
-      />
       <DenyVerificationRequestModal
         show={showDenyModal}
         action={denyType}
