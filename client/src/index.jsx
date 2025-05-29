5b68c721d3c5782bb9284e1dc9158d454c86f2ff
Jake Turner <jturner@cosmistack.com>
2025-05-29 16:04:02 -0700

    feat: react 18 upgrade

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Platform from './Platform';
-import 'semantic-ui-css/semantic.min.css';
-import './styles/index.css';
+import React from "react";
+import Platform from "./Platform";
+import { createRoot } from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import "./styles/index.css";
 
 /**
  * Main React entrypoint
  */
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Platform />);
+root.render(
+  <React.StrictMode>
+    <Platform />
+  </React.StrictMode>
+);
