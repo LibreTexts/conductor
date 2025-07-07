47bf5bcaa9c14c51cf8f402bb9eb2c4b8f87b0b8
Jake Turner <jturner@cosmistack.com>
2025-07-07 03:18:45 -0700

    fix(Store): sync and checkout fixes

--- client/src/screens/conductor/store/catalog.tsx
+++ client/src/screens/conductor/store/catalog.tsx
@@ -155,7 +155,7 @@ export default function CatalogPage() {
       let optionsString = "";
       section.options.forEach((option) => {
         if (option.checked) {
-          optionsString += `,${option.value}`;
+          optionsString += `${optionsString ? "," : ""}${option.value}`;
         }
       });
       if (optionsString) {
