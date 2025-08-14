8d1178f046ef18b703ff13d3de364dc76c799405
Jake Turner <jturner@cosmistack.com>
2025-08-14 16:33:23 -0700

    feat(CentralIdentity): enhanced admin consoles UI

--- client/src/screens/conductor/controlpanel/CentralIdentity/CentralIdentityInstructorVerifications.tsx
+++ client/src/screens/conductor/controlpanel/CentralIdentity/CentralIdentityInstructorVerifications.tsx
@@ -1,14 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import {
-  Header,
-  Segment,
-  Grid,
-  Breadcrumb,
-  Table,
-  Icon,
-  Button,
-} from "semantic-ui-react";
+import { Header, Segment, Grid, Breadcrumb } from "semantic-ui-react";
 import { CentralIdentityVerificationRequest } from "../../../../types";
 import useGlobalError from "../../../../components/error/ErrorHooks";
 import { PaginationWithItemsSelect } from "../../../../components/util/PaginationWithItemsSelect";
@@ -16,8 +8,8 @@ import ManageVerificationRequestModal from "../../../../components/controlpanel/
 import { format as formatDate, parseISO } from "date-fns";
 import { useQuery } from "@tanstack/react-query";
 import api from "../../../../api";
-import LoadingSpinner from "../../../../components/LoadingSpinner";
 import { useModals } from "../../../../context/ModalContext";
+import SupportCenterTable from "../../../../components/support/SupportCenterTable";
 
 const CentralIdentityInstructorVerifications = () => {
   //Global State & Hooks
@@ -25,23 +17,22 @@ const CentralIdentityInstructorVerifications = () => {
   const { openModal, closeAllModals } = useModals();
 
   const [page, setPage] = useState<number>(1);
-  const [limit, setLimit] = useState<number>(10);
-  const { data, isFetching, refetch } = useQuery<{ requests: CentralIdentityVerificationRequest[], totalCount: number }>({
-    queryKey: ['central-identity-verification-requests', page, limit],
+  const [limit, setLimit] = useState<number>(25);
+  const { data, isLoading, refetch } = useQuery<{
+    requests: CentralIdentityVerificationRequest[];
+    totalCount: number;
+  }>({
+    queryKey: ["central-identity-verification-requests", page, limit],
     queryFn: () => loadData(),
-  })
-
-  const TABLE_COLS = [
-    { key: "firstName", text: "First Name" },
-    { key: "lastName", text: "Last Name" },
-    { key: "email", text: "Email" },
-    { key: "requestDate", text: "Request Date" },
-    { key: "Actions", text: "Actions" },
-  ];
+  });
 
   async function loadData() {
     try {
-      const response = await api.getCentralIdentityVerificationRequests({ page, limit, status: "open" });
+      const response = await api.getCentralIdentityVerificationRequests({
+        page,
+        limit,
+        status: "open",
+      });
       if (response.data.err) {
         console.error(response.data.errMsg);
         throw new Error(response.data.errMsg);
@@ -63,7 +54,7 @@ const CentralIdentityInstructorVerifications = () => {
         onClose={handleCloseManageModal}
         onSave={handleCloseManageModal}
       />
-    )
+    );
   }
 
   function handleCloseManageModal() {
@@ -98,13 +89,12 @@ const CentralIdentityInstructorVerifications = () => {
                 </Breadcrumb.Section>
               </Breadcrumb>
             </Segment>
-            {
-              isFetching && <LoadingSpinner />
-            }
             <Segment>
               <PaginationWithItemsSelect
                 activePage={page}
-                totalPages={data?.totalCount ? Math.ceil(data?.totalCount / limit) : 1}
+                totalPages={
+                  data?.totalCount ? Math.ceil(data?.totalCount / limit) : 1
+                }
                 itemsPerPage={limit}
                 setItemsPerPageFn={setLimit}
                 setActivePageFn={setPage}
@@ -112,66 +102,41 @@ const CentralIdentityInstructorVerifications = () => {
               />
             </Segment>
             <Segment>
-              <Table striped celled selectable>
-                <Table.Header>
-                  <Table.Row>
-                    {TABLE_COLS.map((item) => (
-                      <Table.HeaderCell key={item.key}>
-                        <span>{item.text}</span>
-                      </Table.HeaderCell>
-                    ))}
-                  </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {data && data.requests.length > 0 &&
-                    data.requests.map((req) => {
-                      return (
-                        <Table.Row key={req.id} className="word-break-all">
-                          <Table.Cell>
-                            <span>{req.user.first_name}</span>
-                          </Table.Cell>
-                          <Table.Cell>
-                            <span>{req.user.last_name}</span>
-                          </Table.Cell>
-                          <Table.Cell>
-                            <span>{req.user.email}</span>
-                          </Table.Cell>
-                          <Table.Cell>
-                            <span>
-                              {formatDate(
-                                parseISO(req.created_at.toString() ?? ""),
-                                "MM/dd/yyyy"
-                              )}
-                            </span>
-                          </Table.Cell>
-                          <Table.Cell>
-                            <Button
-                              color="blue"
-                              onClick={() => handleSelectRequest(req)}
-                            >
-                              <Icon name="eye" />
-                              View Request
-                            </Button>
-                          </Table.Cell>
-                        </Table.Row>
-                      );
-                    })}
-                  {data && data.requests.length === 0 && (
-                    <Table.Row>
-                      <Table.Cell colSpan={TABLE_COLS.length + 1}>
-                        <p className="text-center">
-                          <em>No results found.</em>
-                        </p>
-                      </Table.Cell>
-                    </Table.Row>
-                  )}
-                </Table.Body>
-              </Table>
+              <SupportCenterTable<CentralIdentityVerificationRequest>
+                data={data?.requests ?? []}
+                loading={isLoading}
+                onRowClick={(record) => handleSelectRequest(record)}
+                columns={[
+                  {
+                    accessor: "user.first_name",
+                    title: "First Name",
+                  },
+                  {
+                    accessor: "user.last_name",
+                    title: "Last Name",
+                  },
+                  {
+                    accessor: "user.email",
+                    title: "Email",
+                  },
+                  {
+                    accessor: "created_at",
+                    title: "Request Date",
+                    render: (record) =>
+                      formatDate(
+                        parseISO(record.created_at.toString() ?? ""),
+                        "MM/dd/yyyy"
+                      ),
+                  },
+                ]}
+              />
             </Segment>
             <Segment>
               <PaginationWithItemsSelect
                 activePage={page}
-                totalPages={data?.totalCount ? Math.ceil(data?.totalCount / limit) : 1}
+                totalPages={
+                  data?.totalCount ? Math.ceil(data?.totalCount / limit) : 1
+                }
                 itemsPerPage={limit}
                 setItemsPerPageFn={setLimit}
                 setActivePageFn={setPage}
