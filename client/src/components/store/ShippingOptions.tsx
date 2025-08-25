0b4a9e76a1f187f540810258606e77bd56156996
Jake Turner <jturner@cosmistack.com>
2025-08-25 12:22:23 -0700

    feat(Store): show shipping times as estimated delivery dates

--- client/src/components/store/ShippingOptions.tsx
+++ client/src/components/store/ShippingOptions.tsx
@@ -41,39 +41,68 @@ const ShippingOptions: React.FC<ShippingOptionsProps> = ({
               onChange={setSelectedShippingOption}
               className="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4"
             >
-              {shippingOptions.map((deliveryMethod) => (
-                <Radio
-                  key={deliveryMethod.id}
-                  value={deliveryMethod}
-                  aria-label={deliveryMethod.title}
-                  aria-description={`${deliveryMethod.total_days_min} - ${
-                    deliveryMethod.total_days_max
-                  } business days for ${formatPrice(
-                    deliveryMethod.cost_excl_tax,
-                    false
-                  )}`}
-                  className="group relative flex cursor-pointer rounded-md border border-gray-300 bg-white p-4 shadow-sm focus:outline-none data-[checked]:border-transparent data-[focus]:ring-2 data-[focus]:ring-indigo-500"
-                >
-                  <span className="flex flex-1">
-                    <span className="flex flex-col">
-                      <span className="block text-sm font-medium text-gray-900">
-                        {deliveryMethod.title}
-                      </span>
-                      <span className="mt-1 flex items-center text-sm text-gray-500">
-                        {deliveryMethod.total_days_min} -{" "}
-                        {deliveryMethod.total_days_max} business days
-                      </span>
-                      <span className="mt-6 text-sm font-medium text-gray-900">
-                        {formatPrice(deliveryMethod.cost_excl_tax, true)}
+              {shippingOptions.map((deliveryMethod) => {
+                const minDeliveryDate = new Date();
+                minDeliveryDate.setDate(
+                  minDeliveryDate.getDate() + deliveryMethod.total_days_min
+                );
+                const maxDeliveryDate = new Date();
+                maxDeliveryDate.setDate(
+                  maxDeliveryDate.getDate() + deliveryMethod.total_days_max
+                );
+
+                const minDeliverDateFormatted = new Intl.DateTimeFormat(
+                  "en-US",
+                  {
+                    month: "numeric",
+                    day: "numeric",
+                    year: "numeric",
+                  }
+                ).format(minDeliveryDate);
+
+                const maxDeliverDateFormatted = new Intl.DateTimeFormat(
+                  "en-US",
+                  {
+                    month: "numeric",
+                    day: "numeric",
+                    year: "numeric",
+                  }
+                ).format(maxDeliveryDate);
+
+                return (
+                  <Radio
+                    key={deliveryMethod.id}
+                    value={deliveryMethod}
+                    aria-label={deliveryMethod.title}
+                    aria-description={`${deliveryMethod.total_days_min} - ${
+                      deliveryMethod.total_days_max
+                    } business days for ${formatPrice(
+                      deliveryMethod.cost_excl_tax,
+                      false
+                    )}`}
+                    className="group relative flex cursor-pointer rounded-md border border-gray-300 bg-white p-4 shadow-sm focus:outline-none data-[checked]:border-transparent data-[focus]:ring-2 data-[focus]:ring-indigo-500"
+                  >
+                    <span className="flex flex-1">
+                      <span className="flex flex-col">
+                        <span className="block text-sm font-medium text-gray-900">
+                          {deliveryMethod.title}
+                        </span>
+                        <span className="mt-1 flex items-center text-sm text-gray-500">
+                          Est. Delivery {minDeliverDateFormatted} -{" "}
+                          {maxDeliverDateFormatted}
+                        </span>
+                        <span className="mt-6 text-sm font-medium text-gray-900">
+                          {formatPrice(deliveryMethod.cost_excl_tax, true)}
+                        </span>
                       </span>
                     </span>
-                  </span>
-                  <span
-                    aria-hidden="true"
-                    className="pointer-events-none absolute -inset-px rounded-lg border-2 border-transparent group-data-[focus]:border group-data-[checked]:border-indigo-500"
-                  />
-                </Radio>
-              ))}
+                    <span
+                      aria-hidden="true"
+                      className="pointer-events-none absolute -inset-px rounded-lg border-2 border-transparent group-data-[focus]:border group-data-[checked]:border-indigo-500"
+                    />
+                  </Radio>
+                );
+              })}
             </RadioGroup>
           </fieldset>
           <p className="mt-4 text-sm text-gray-500 text-center">
