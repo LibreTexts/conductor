015b2347900378a3c7c92f52082083a7c1b3924f
Jake Turner <jturner@cosmistack.com>
2025-10-06 13:37:58 -0700

    fix: instructor verification link

--- server/api/services/client-config-service.ts
+++ server/api/services/client-config-service.ts
@@ -6,6 +6,7 @@ export default class ClientConfigService {
         return {
             env: process.env.CLIENT__ENV || process.env.NODE_ENV,
             main_commons_url: process.env.CLIENT__MAIN_COMMONS_URL || "",
+            instructor_verification_url: process.env.CLIENT__INSTRUCTOR_VERIFICATION_URL || "",
         };
     }
 }
