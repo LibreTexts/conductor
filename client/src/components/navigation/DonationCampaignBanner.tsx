5cb47852bb2696e522bfe7b2c72aa62f198ec976
Jake Turner <jturner@cosmistack.com>
2025-12-04 11:56:03 -0800

    fix(DonationCampaignBanner): change bg color

--- client/src/components/navigation/DonationCampaignBanner.tsx
+++ client/src/components/navigation/DonationCampaignBanner.tsx
@@ -11,20 +11,21 @@ const DonationCompaignBanner: React.FC<DonationCompaignBannerProps> = ({
   return (
     <div
       className={classNames(
-        "flex items-center justify-center bg-primary-dark px-8 py-2.5 w-full h-10",
+        "flex items-center justify-center bg-[#C2410C] px-8 py-2.5 w-full h-10",
         className
       )}
     >
       <IconHeartHandshake className="text-white mr-2" />
-      <p className="text-xs lg:text-base text-white">
-        <strong className="font-semibold">Global Translation for Good: </strong>
-        Help us translate LibreTexts OER into more languages! Donate today through{" "}
+      <p className="text-xs lg:text-base text-white font-semibold">
+        Global Translation for Good: Help us translate LibreTexts OER into more
+        languages! Donate today through{" "}
         <a
           href="https://donorbox.org/libretexts-global-translation-for-good"
           className="underline text-white hover:text-white"
         >
           Donorbox
-        </a>.
+        </a>
+        .
       </p>
     </div>
   );
