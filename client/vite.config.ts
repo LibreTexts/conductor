585e8dd62fc978816b087a7f8110045ae20e6543
Akhileshwar Shriram <112577383+AkhilTheBoss@users.noreply.github.com>
2026-03-18 14:02:39 -0700

    refactor: changed to tailwind v4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [tailwindcss(), react(), viteTsconfigPaths()],
   resolve: {
     alias: {
       // https://github.com/tabler/tabler-icons/issues/1233#issuecomment-2428245119
