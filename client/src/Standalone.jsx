455dbaa6c86f7532819c3a04df5e74a3938d63ab
jakeaturner <jturner@cosmistack.com>
2026-04-30 11:39:22 -0700

    fix(UI): consistent SkipLink usage

--- client/src/Standalone.jsx
+++ client/src/Standalone.jsx
@@ -1,34 +1,51 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import AdoptionReportPage from './components/adoptionreport/AdoptionReportPage';
-import AccessibilityStatement from './components/util/AccessibilityStatement';
-import TranslationFeedbackExport from './components/util/TranslationFeedbackExport';
+import { Switch, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
+import AdoptionReportPage from "./components/adoptionreport/AdoptionReportPage";
+import AccessibilityStatement from "./components/util/AccessibilityStatement";
+import TranslationFeedbackExport from "./components/util/TranslationFeedbackExport";
+import { SkipLink } from "@libretexts/davis-react";
 
 /* 404 */
-import PageNotFound from './components/util/PageNotFound';
+import PageNotFound from "./components/util/PageNotFound";
 
 /**
  * Standalone pages that do not fall under the Commons or Conductor trees.
  */
 const Standalone = () => {
-
   // Global State
   const org = useSelector((state) => state.org);
 
   return (
     <div className="standalone">
-      <Switch>
-        {(org.orgID === 'libretexts') && [
-          <Route exact path="/adopt" key="adoptionreport" component={AdoptionReportPage} />,
-          <Route exact path="/accessibility" key="accessibility" component={AccessibilityStatement} />,
-          <Route exact path="/translationfeedbackexport" key="translationfeedback" component={TranslationFeedbackExport} />,
-        ]}
-        {/* Fallback for non-LibreTexts instances */}
-        <Route component={PageNotFound} />
-      </Switch>
+      <SkipLink targetId="main-content" />
+      <main id="main-content">
+        <Switch>
+          {org.orgID === "libretexts" && [
+            <Route
+              exact
+              path="/adopt"
+              key="adoptionreport"
+              component={AdoptionReportPage}
+            />,
+            <Route
+              exact
+              path="/accessibility"
+              key="accessibility"
+              component={AccessibilityStatement}
+            />,
+            <Route
+              exact
+              path="/translationfeedbackexport"
+              key="translationfeedback"
+              component={TranslationFeedbackExport}
+            />,
+          ]}
+          {/* Fallback for non-LibreTexts instances */}
+          <Route component={PageNotFound} />
+        </Switch>
+      </main>
     </div>
-  )
+  );
 };
 
 export default Standalone;
