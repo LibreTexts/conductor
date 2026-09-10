d951848783225646ade0f952c312645b8bd2d12b
Yashar Ghaemi <yashar.ghaemi@antecedent.us>
2026-09-10 14:16:38 -0400

    feat(Glossary): add showTermOnly feature to glossary configuration
    - Introduced a new `showTermOnly` boolean property to the glossary configuration, allowing users to control the display of terms without definitions in external views.
    - Updated relevant components and API endpoints to handle the new property, ensuring consistent behavior across the application.
    - Enhanced the `GlossaryConfigModal` to include a switch for toggling the `showTermOnly` setting, improving user interaction and configuration options.

--- client/src/api.ts
+++ client/src/api.ts
@@ -2858,14 +2858,16 @@ class API {
     mode: GlossaryConfigMode;
     glossaryPageId?: string;
     groups: GlossaryConfigGroup[];
+    showTermOnly: boolean;
   }) {
-    const { library, coverID, mode, glossaryPageId, groups } = props;
+    const { library, coverID, mode, glossaryPageId, groups, showTermOnly } = props;
     const res = await axios.put<
       { config: GlossaryConfig } & ConductorBaseResponse
     >(`/commons/book/${library}/${coverID}/glossary-config`, {
       mode,
       glossaryPageId,
       groups,
+      showTermOnly,
     });
     return res.data;
   }
